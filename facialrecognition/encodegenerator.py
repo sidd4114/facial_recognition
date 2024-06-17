24184774e2acd1b37e80a597c92dd96601502486
sid4114 <siddhenpise10@gmail.com>
2024-06-17 15:18:12 +0530

    changes

---
+++ facialrecognition/encodegenerator.py
@@ -0,0 +1,42 @@
+import cv2
+import pickle
+import os
+import face_recognition
+
+#importing images of students into list
+foldermodepath=r'facialrecognition\images'
+
+pathlist=os.listdir(foldermodepath)    #list of stud imgs                                                #a list containing imgs
+
+imglist=[] #empty list
+studentid=[] #empty list for id
+
+for path in pathlist:
+  imglist.append(cv2.imread(os.path.join(foldermodepath,path)))   #joining path
+  #os.path.splitext(path,[0])  #spilting id and .png and getting id
+  studentid.append(os.path.splitext(path)[0])
+
+
+#print(studentid)  #printing student ids
+#print(len(imglist))
+
+
+
+def findencodings(imageslist):
+  encodelist=[]  #creating empty list for encodeings
+  for img in imageslist:
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)      #converting bgr to rgb since opencv uses bgr and facerecog uses 
+    encode=face_recognition.face_encodings(img)  #finding encodings
+    encodelist.append(encode)
+
+  return encodelist
+
+print("encoding started")
+encodelistknown=findencodings(imglist) #generating imgs
+encodelistknownwithids=[encodelistknown,studentid]         #storing encodings with resp student id
+print("encoding comlete")
+
+file=open("encodefile.p",'wb')  #opening file(writing in binary)
+pickle.dump(encodelistknownwithids,file)
+file.close()
+print("file saved")
--- facialrecognition/facialrecognition.py
+++ facialrecognition/facialrecognition.py
@@ -1,16 +1,48 @@
 import cv2
+import os
+import pickle
+import face_recognition
 
-cap=cv2.VideoCapture(0)
+
+cap=cv2.VideoCapture(0)  #accessing webcam
 
 cap.set(3,640)                              #setting width
 cap.set(4,480)                               #setting height
 
-imgbackground=cv2.imread(r"C:\Users\siddh\facial_recognition\facialrecognition\resources\background.png")
+imgbackground=cv2.imread(r"C:\Users\siddh\facial_recognition\facialrecognition\resources\background.png")  #background img
+
+#importing mode images into list
+foldermodepath=r'facialrecognition\resources\Modes'
+
+modepathlist=os.listdir(foldermodepath)                                                    #a list containing imgs
+
+imgmodelist=[] 
+
+for path in modepathlist:
+  imgmodelist.append(cv2.imread(os.path.join(foldermodepath,path)))   #joining path
+  
+  
+#load the encoding file
+print("loading encoded file...")
+file=open("encodefile.p",'rb')             
+encodelistknownwithids=pickle.load(file)   #loading contents
+file.close()
+encodelistknown,studentid=encodelistknownwithids #seperating ids and encoding
+print("encoded file loaded")
 
 while True:
     success,img=cap.read()
 
+    imgs=cv2.resize(img,(0,0,None,0.25,0.25))   #resizing image
+    imgs=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)    #converting into rgb
+    
+    facecurframe=face_recognition.face_locations(imgs)                              # detects the locations of faces in the provided image (imgs) and returns a list of tuples. Each tuple represents the coordinates of a detected face in the format
+    
+    encodecurframe=face_recognition.face_encodings(imgs,facecurframe)
+
     imgbackground[162:162+480,55:55+640]=img   #slicing the background
+    imgbackground[44:44+633,808:808+414]=imgmodelist[1]
+    
 
 
     #cv2.imshow("webcam",img)
